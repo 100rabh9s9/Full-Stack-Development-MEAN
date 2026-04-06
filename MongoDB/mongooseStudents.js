901cb3064c3776712daa4db7697141635e6bb31d
SOURABH S <100rabh9s9@gmail.com>
2026-04-06 20:46:44 +0530

    Insert using Mongoose

---
+++ MongoDB/mongooseStudents.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/college').then(() => console.log("MongoDB Connected")).catch((err) => {console.log(err)});
+const studentSchema = new mongoose.Schema({
+    rollno: Number,
+    name: String,
+    course:String,
+    semester:Number,
+    marks: Number
+});
+const Student = mongoose.model('Student', studentSchema);   
+
+const insertStudents = async () => {
+    await Student.insertMany([
+        {rollno:101,name:"Anu",course:"MCA",semester:1,marks:85},
+        {rollno:102,name:"Rahul",course:"MCA",semester:1,marks:78}
+    ]);
+    console.log("Students record inserted Successfully");
+};
+insertStudents();
