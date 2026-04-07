6e358342228afb62d9a63376dcf6e3cb3e754838
SOURABH S <100rabh9s9@gmail.com>
2026-04-07 15:03:42 +0530

    Updated Comments

--- MongoDB/mongooseStudents.js
+++ MongoDB/mongooseStudents.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/college').then(() => console.log("MongoDB Connected")).catch((err) => {console.log(err)});
+//This defines the structure of Students collection in MongoDB
 const studentSchema = new mongoose.Schema({
     rollno: Number,
     name: String,
@@ -8,7 +9,7 @@ const studentSchema = new mongoose.Schema({
     marks: Number
 });
 const Student = mongoose.model('Student', studentSchema);   
-
+//Insert Student Records
 const insertStudents = async () => {
     await Student.insertMany([
         {rollno:101,name:"Anu",course:"MCA",semester:1,marks:85},
