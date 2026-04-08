ef7e65a2524e70848169f6020593f51807fe7393
SOURABH S <100rabh9s9@gmail.com>
2026-04-08 10:51:08 +0530

    Mongo DB Program to Delete all records

---
+++ MongoDB/delete.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/college').then(() => console.log("MongoDB Connected")).catch((err) => {console.log(err)});
+//This defines the structure of Students collection in MongoDB
+const studentSchema = new mongoose.Schema({
+    rollno: Number,
+    name: String,
+    course:String,
+    semester:Number,
+    marks: Number
+});
+const Student = mongoose.model('Student', studentSchema); 
+//Delete Student Record
+Student.deleteOne({rollno:101})
+.then(()=>{
+    console.log("Student Record Deleted Successfully");
+    mongoose.connection.close();    
+});
